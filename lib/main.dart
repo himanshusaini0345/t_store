aa7e45c9ffd0c19c101bee9eb0fad1991548af7a
himanshu saini <himanshusaini0345@outlook.com>
2024-12-14 19:48:06 +0530

    theme setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,1 @@
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
-
+import 'package:flutter/material.dart';import 'package:t_store/app.dart';void main() {  runApp(const App());}
