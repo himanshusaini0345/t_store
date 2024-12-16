6f218014edac7a6baad4dd7a45d7d27dfe207d75
himanshu saini <himanshusaini0345@outlook.com>
2024-12-16 17:32:21 +0530

    onboarding screen

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -4,7 +4,7 @@ class TTexts {
 // OnBoarding Texts
   static const String onBoardingTitle1 = "Choose your product";
   static const String onBoardingTitle2 = "Select Payment Method";
-  static const String onboardingTitle3 = "Deliver at your door step";
+  static const String onBoardingTitle3 = "Deliver at your door step";
 
   static const String onBoardingSubTitle1 =
       "Welcome to a World of Limitless Choices Your Perfect Product Awaits!";
